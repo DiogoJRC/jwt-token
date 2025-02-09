30952e814373646fe5b0a6c8a56f7371c88229f8
Diogo Capdeville <diogoj.r.c@hotmail.com>
2025-02-08 23:18:25 -0300

    Criando o método de validação de tokens

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import { sign } from "./jwt/sign";
+import { verify } from "./jwt/verify";
+
+const secret = "#chave";
 
 const token = sign({
   exp: Date.now() + 24 * 60 * 60 * 1000,
   data: {
     sub: "@diogojrc",
   },
-  secret: "",
+  secret,
 });
 
-console.log(token);
+const decoded = verify({
+  token,
+  secret,
+});
+
+console.log({ decoded });
--- src/jwt/sign.ts
+++ src/jwt/sign.ts
@@ -1,4 +1,4 @@
-import { createHmac } from "crypto";
+import { generateSignature } from "./generateSignature";
 
 interface ISingOptions {
   exp: number;
@@ -6,16 +6,16 @@ interface ISingOptions {
   secret: string;
 }
 
-export function sign(options: ISingOptions) {
+export function sign({ exp, data, secret }: ISingOptions) {
   const header = {
     alg: "HS256",
     typ: "JWT",
   };
 
   const payload = {
-    ...options.data,
+    ...data,
     iat: Date.now(),
-    exp: options.exp,
+    exp,
   };
 
   const base64EncodedHeader = Buffer.from(JSON.stringify(header)).toString(
@@ -26,10 +26,11 @@ export function sign(options: ISingOptions) {
     "base64url"
   );
 
-  const hmac = createHmac("sha256", options.secret);
-  const signature = hmac
-    .update(`${base64EncodedHeader}.${base64EncodedPayload}`)
-    .digest("base64url");
+  const signature = generateSignature({
+    header: base64EncodedHeader,
+    payload: base64EncodedPayload,
+    secret,
+  });
 
   return `${base64EncodedHeader}.${base64EncodedPayload}.${signature}`;
 }
